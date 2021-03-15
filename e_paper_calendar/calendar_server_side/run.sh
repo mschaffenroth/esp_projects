90c9ee383fd0183d9609ecbc293bbe45cdbe1e74
Moritz <moritz@mschaffenroth.de>
2021-03-15 11:05:11 +0100

    add timezone

--- e_paper_calendar/calendar_server_side/run.sh
+++ e_paper_calendar/calendar_server_side/run.sh
@@ -1,3 +1,3 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-sudo docker run --rm -v $DIR/files:/usr/src/app/files/ esp_frame:latest
+sudo docker run --rm -e TZ="Europe/Berlin" -v $DIR/files:/usr/src/app/files/ esp_frame:latest
 cp $DIR/files/* $DIR/../../../webfacade/static_files/
